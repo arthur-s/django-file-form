2a450f24d1b501d619a8c6b6212f13c8953a5fea
A.Sultanbekov <sultan@srv001.ex-tender.com>
2015-09-18 16:54:31 +0600

    FileStorage by dafault

--- django_file_form/conf.py
+++ django_file_form/conf.py
@@ -4,5 +4,5 @@ from django.conf import settings
 # Must the client be logged in for an upload or delete?
 MUST_LOGIN = getattr(settings, 'FILE_FORM_MUST_LOGIN', False)
 
-STORAGE = getattr(settings, 'FILE_FORM_STORAGE', settings.DEFAULT_FILE_STORAGE)
+STORAGE = getattr(settings, 'FILE_FORM_STORAGE', 'django.core.files.storage.FileSystemStorage')
 STORAGE_KWARGS = getattr(settings, 'FILE_FORM_STORAGE_KWARGS', {'location': settings.MEDIA_ROOT})
